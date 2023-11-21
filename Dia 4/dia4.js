68487863897dd54bb6973f92aeffce118e2919df
Enzo Soares <131898585+enzosoaress@users.noreply.github.com>
2023-11-21 16:42:17 -0300

    Ex 1 - Verificando disponibilidade para comer fora

--- Dia 4/dia4.js
+++ Dia 4/dia4.js
@@ -1,14 +1,15 @@
-// let fome = prompt("Você está com fome?(s/n")
-// let dinheiro = prompt("Você tem dinheiro para comer?(s/n)")
-// let restaurante = prompt("O restaurante está aberto?(s/n)")
+//=========== PRIMEIRO EXERCÍCIO ==============
+let fome = prompt("Você está com fome?(s/n")
+ let dinheiro = prompt("Você tem dinheiro para comer?(s/n)")
+ let restaurante = prompt("O restaurante está aberto?(s/n)")
 
-// if(fome === "n" || dinheiro === "n"){
-//     console.log("Hoje a janta será em casa")
-// }else if(fome === "s" && dinheiro === "s" && restaurante === "n"){
-//     console.log("Peça um delivery")
-// }else{(fome === "s" && dinheiro === "s" && restaurante === "s")
-//     console.log("Hoje o jantar será no seu restaurante preferido!")
-// }
+ if(fome === "n" || dinheiro === "n"){
+     console.log("Hoje a janta será em casa")
+ }else if(fome === "s" && dinheiro === "s" && restaurante === "n"){
+     console.log("Peça um delivery")
+ }else{(fome === "s" && dinheiro === "s" && restaurante === "s")
+     console.log("Hoje o jantar será no seu restaurante preferido!")
+ }
 
 //============ SEGUNDO EXERCÍCIO ============
 
